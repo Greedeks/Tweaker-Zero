219ba26cec7d25b756b5844904c9c3978b1ed0b8
greedeks <greedeks@gmail.com>
2021-09-05 18:51:08 +0300

    info

--- Tweaker/Properties/AssemblyInfo.cs
+++ Tweaker/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.1.2.0")]
+[assembly: AssemblyFileVersion("2.1.2.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
