94bb77bdbfd5711304c807d635381b2e2e25b895
greedeks <greedeks@gmail.com>
2021-09-06 19:53:59 +0300

    timer

--- Tweaker/MessageForUser.xaml.cs
+++ Tweaker/MessageForUser.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Tweaker
 {
@@ -19,9 +20,25 @@ namespace Tweaker
     /// </summary>
     public partial class MessageForUser : Window
     {
+        DispatcherTimer _timer;
+        TimeSpan _time; 
+
         public MessageForUser()
         {
             InitializeComponent();
+
+            #region Таймер закрытия окна
+            _time = TimeSpan.FromSeconds(9);
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                string _texttimer = _time.ToString("ss");
+                TextTimer.Content = "Автозакрытия  окна через "+_texttimer+" секунд.";
+                if (_time == TimeSpan.Zero) { _timer.Stop(); this.Close(); }
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, Application.Current.Dispatcher);
+
+            _timer.Start();
+            #endregion
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
