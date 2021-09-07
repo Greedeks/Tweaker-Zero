45964b9f3acc4dd0ce134100348aeee8165a0a0e
greedeks <greedeks@gmail.com>
2021-09-07 22:50:45 +0300

    fix

--- Tweaker/App.xaml.cs
+++ Tweaker/App.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Tweaker
 {
     public partial class App : Application
     {
-        public App() => AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Exception);
-
-        static void Exception(object sender, UnhandledExceptionEventArgs args)
-        {
-            Exception e = (Exception)args.ExceptionObject;
-            MessageBox.Show(e.ToString());
-        }
     }
 }
--- Tweaker/CheakApplicationCopy.cs
+++ Tweaker/CheakApplicationCopy.cs
@@ -3,7 +3,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.Windows;
 
 namespace Tweaker
 {
