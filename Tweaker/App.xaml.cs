eaee0856675263249fa2018d7a0e1979ab19b24d
greedeks <greedeks@gmail.com>
2021-09-07 20:49:24 +0300

    Global Cath

--- Tweaker/App.xaml.cs
+++ Tweaker/App.xaml.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Tweaker
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App() => AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Exception);
+
+        static void Exception(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            MessageBox.Show(e.ToString());
+        }
     }
 }
