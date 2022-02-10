0f5159b421669235c100077769ee9bee37aadc57
greedeks <greedeks@gmail.com>
2022-02-10 19:41:39 +0300

    test

--- Tweaker/Сlasses/SettingsWindows.cs
+++ Tweaker/Сlasses/SettingsWindows.cs
@@ -73,9 +73,11 @@ namespace Tweaker.Сlasses
             }
 
             //#4
-            _counTasks=0;
-            TaskCheckState(@"""Microsoft\Windows\Maintenance\WinSAT""", @"""Microsoft\Windows\Autochk\Proxy""", @"""Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser""", @"""Microsoft\Windows\Application Experience\ProgramDataUpdater""", @"""Microsoft\Windows\Application Experience\StartupAppTask""", @"""Microsoft\Windows\PI\Sqm-Tasks""",
-                @"""Microsoft\Windows\NetTrace\GatherNetworkInfo""", @"""Microsoft\Windows\Customer Experience Improvement Program\Consolidator""", @"""Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask""", @"""Microsoft\Windows\Customer Experience Improvement Program\UsbCeip""", @"""Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticResolver""", @"""Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector""");
+            TaskCheckState(@"""Microsoft\Windows\Maintenance\WinSAT""", @"""Microsoft\Windows\Autochk\Proxy""", @"""Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser""", 
+                @"""Microsoft\Windows\Application Experience\ProgramDataUpdater""", @"""Microsoft\Windows\Application Experience\StartupAppTask""", @"""Microsoft\Windows\PI\Sqm-Tasks""",
+                @"""Microsoft\Windows\NetTrace\GatherNetworkInfo""", @"""Microsoft\Windows\Customer Experience Improvement Program\Consolidator""", @"""Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask""", 
+                @"""Microsoft\Windows\Customer Experience Improvement Program\UsbCeip""", @"""Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticResolver""", @"""Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector""");
+
             if (_counTasks>0)
             {
                 confidentiality.TButton4.State = true;
@@ -86,7 +88,91 @@ namespace Tweaker.Сlasses
                 confidentiality.TButton4.State = false;
                 confidentiality.Tweak4.Style = (Style)Application.Current.Resources["Tweaks_OFF"];
             }
-            
+
+            //#5
+            _key[12] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\AppCompat");
+
+            if(_key[12] != null && _key[12].GetValue("DisableInventory").ToString() != "1")
+            {
+                confidentiality.TButton5.State = true;
+                confidentiality.Tweak5.Style = (Style)Application.Current.Resources["Tweaks_ON"];
+            }
+            else
+            {
+                confidentiality.TButton5.State = false;
+                confidentiality.Tweak5.Style = (Style)Application.Current.Resources["Tweaks_OFF"];
+            }
+
+            //#6
+            _key[13] = localMachineKey.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection");
+            _key[14] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\AppCompat");
+            _key[15] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\DataCollection");
+            _key[16] = currentUserKey.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced");
+
+            if(_key[13] != null && _key[13].GetValue("AllowTelemetry").ToString() != "0" || _key[14] != null && _key[14].GetValue("AITEnable").ToString() != "0" ||
+            _key[15] != null && _key[15].GetValue("AllowDeviceNameInTelemetry").ToString() != "0" || _key[16] != null && _key[16].GetValue("Start_TrackProgs").ToString() != "0")
+            {
+                confidentiality.TButton6.State = true;
+                confidentiality.Tweak6.Style = (Style)Application.Current.Resources["Tweaks_ON"];
+            }
+            else
+            {
+                confidentiality.TButton6.State = false;
+                confidentiality.Tweak6.Style = (Style)Application.Current.Resources["Tweaks_OFF"];
+            }
+
+            //#7
+            _key[17] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\TabletPC");
+            _key[18] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\HandwritingErrorReports");
+            _key[19] = currentUserKey.OpenSubKey(@"Software\Microsoft\Input\TIPC");
+
+            if (_key[17] != null && _key[17].GetValue("PreventHandwritingDataSharing").ToString() != "1" || _key[18] != null && _key[18].GetValue("PreventHandwritingErrorReports").ToString() != "1" ||
+            _key[19] != null && _key[19].GetValue("Enabled").ToString() != "0")
+            {
+                confidentiality.TButton7.State = true;
+                confidentiality.Tweak7.Style = (Style)Application.Current.Resources["Tweaks_ON"];
+            }
+            else
+            {
+                confidentiality.TButton7.State = false;
+                confidentiality.Tweak7.Style = (Style)Application.Current.Resources["Tweaks_OFF"];
+            }
+
+            //#8
+            _key[20] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\AppCompat");
+            _key[21] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\Personalization");
+
+            if (_key[20] != null && _key[20].GetValue("DisableUAR").ToString() != "1" || _key[21] != null && _key[21].GetValue("NoLockScreenCamera").ToString() != "1")
+            {
+                confidentiality.TButton8.State = true;
+                confidentiality.Tweak8.Style = (Style)Application.Current.Resources["Tweaks_ON"];
+            }
+            else
+            {
+                confidentiality.TButton8.State = false;
+                confidentiality.Tweak8.Style = (Style)Application.Current.Resources["Tweaks_OFF"];
+            }
+
+            //#9
+            _key[22] = localMachineKey.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\LocationAndSensors");
+
+            //if (_key[22] != null && _key[22].GetValue("DisableLocationScripting").ToString() != "1" || _key[22] != null && _key[22].GetValue("DisableLocation").ToString() != "1" ||
+            //_key[22] != null && _key[22].GetValue("DisableWindowsLocationProvider").ToString() != "1")
+            //{
+            //    confidentiality.TButton9.State = true;
+            //    confidentiality.Tweak9.Style = (Style)Application.Current.Resources["Tweaks_ON"];
+            //}
+            //else
+            //{
+            //    confidentiality.TButton9.State = false;
+            //    confidentiality.Tweak9.Style = (Style)Application.Current.Resources["Tweaks_OFF"];
+            //}
+
+            if (Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\LocationAndSensors", "DisableLocation", null) != null && Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\LocationAndSensors", "DisableLocation", null).ToString() != "1")
+            {
+                confidentiality.TButton9.State = true;
+                confidentiality.Tweak9.Style = (Style)Application.Current.Resources["Tweaks_ON"];
+            }
         }
 
         private void TaskCheckState(params string[] TaskName)
@@ -101,6 +187,7 @@ namespace Tweaker.Сlasses
                 StandardOutputEncoding = Encoding.GetEncoding(866),
                 WindowStyle = ProcessWindowStyle.Hidden
             });
+            _counTasks = 0;
             for (int i = 0; i < TaskName.Length; i++)
             {
                  process.StartInfo.Arguments = String.Format("/TN {0}", TaskName[i]);
