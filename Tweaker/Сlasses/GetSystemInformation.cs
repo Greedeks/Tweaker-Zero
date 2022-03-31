92e181e3f798e9a361cadfca6bcf64457da5caa3
greedeks <greedeks@gmail.com>
2022-03-31 21:44:48 +0300

    info pc mem/os/ram

--- Tweaker/Сlasses/GetSystemInformation.cs
+++ Tweaker/Сlasses/GetSystemInformation.cs
@@ -44,10 +44,16 @@ namespace Tweaker.Сlasses
         }
 
         private static List<string> _INFthisPC = new List<string>();
-        private static List<string> _idVideoController = new List<string>();
-        private static string _test = default;
+        private List<string> _idSearch = new List<string>();
+        private string _test = default;
         internal void GetInormationPC()
         {
+            foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Caption, OSArchitecture, Version from Win32_OperatingSystem").Get())
+            {
+                string _caption = (string)managementObj["Caption"];
+                _INFthisPC.Add(_caption.Substring(_caption.IndexOf('W')) + ", " + (string)managementObj["OSArchitecture"] + ", V" +(string)managementObj["Version"]);
+            }
+
             foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name, SerialNumber from Win32_BIOS").Get())
                 _INFthisPC.Add((string)managementObj["Name"] + ", S/N "+(string)managementObj["SerialNumber"]);
 
@@ -57,11 +63,23 @@ namespace Tweaker.Сlasses
             foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name from Win32_Processor").Get())
                 _INFthisPC.Add((string)managementObj["Name"]);
 
+            foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Tag from Win32_PhysicalMemory").Get())
+                _idSearch.Add((string)managementObj["Tag"]);
+            for (int i = 0; i < _idSearch.Count; i++)
+            {
+                foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Manufacturer, Capacity, ConfiguredClockSpeed from Win32_PhysicalMemory where Tag='" + _idSearch[i] + "'").Get())
+                    _test += ((string)managementObj["Manufacturer"] + ", " + Convert.ToString((ulong)managementObj["Capacity"] / 1048576000) + " GB, " + Convert.ToString((uint)managementObj["ConfiguredClockSpeed"]) + " MHz\n");
+            }
+            _INFthisPC.Add(_test);
+
+            _test = string.Empty;
+            _idSearch.Clear();
+
             foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select DeviceID from Win32_VideoController").Get())
-                _idVideoController.Add((string)managementObj["DeviceID"]);
-            for(int i=0; i<_idVideoController.Count; i++) { 
-                        foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name, AdapterRAM from Win32_VideoController where DeviceID='" + _idVideoController[i] + "'").Get())
-                        _test+=((string)managementObj["Name"] + ", " + Convert.ToString(((uint)managementObj["AdapterRAM"] / 1048576) / 1000) + " GB\n");
+                _idSearch.Add((string)managementObj["DeviceID"]);
+            for(int i=0; i< _idSearch.Count; i++) { 
+                        foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name, AdapterRAM from Win32_VideoController where DeviceID='" + _idSearch[i] + "'").Get())
+                        _test+=((string)managementObj["Name"] + ", " + Convert.ToString(((uint)managementObj["AdapterRAM"] / 1048576000)) + " GB\n");
             }
             _INFthisPC.Add(_test);
 
@@ -69,10 +87,12 @@ namespace Tweaker.Сlasses
 
         internal void SetInormationPC(SystemInfromation systemInfromation)
         {
-            systemInfromation.NameBIOS.Text = _INFthisPC[0];
-            systemInfromation.NameMotherBr.Text = _INFthisPC[1];
-            systemInfromation.NameCPU.Text = _INFthisPC[2];
-            systemInfromation.NameGPU.Text = _INFthisPC[3];
+            systemInfromation.NameOS.Text = _INFthisPC[0];
+            systemInfromation.NameBIOS.Text = _INFthisPC[1];
+            systemInfromation.NameMotherBr.Text = _INFthisPC[2];
+            systemInfromation.NameCPU.Text = _INFthisPC[3];
+            systemInfromation.NameRAM.Text = _INFthisPC[4];
+            systemInfromation.NameGPU.Text = _INFthisPC[5];
         }
     }
 }
