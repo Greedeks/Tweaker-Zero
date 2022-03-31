f56a9e663a6ec0ebe9dbb9bcbbac4535cf5f4a07
greedeks <greedeks@gmail.com>
2022-03-31 20:40:22 +0300

    bios/motherboard

--- Tweaker/Сlasses/GetSystemInformation.cs
+++ Tweaker/Сlasses/GetSystemInformation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Management;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using Tweaker.Pages;
@@ -43,21 +44,35 @@ namespace Tweaker.Сlasses
         }
 
         private static List<string> _INFthisPC = new List<string>();
+        private static List<string> _idVideoController = new List<string>();
+        private static string _test = default;
         internal void GetInormationPC()
         {
+            foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name, SerialNumber from Win32_BIOS").Get())
+                _INFthisPC.Add((string)managementObj["Name"] + ", S/N "+(string)managementObj["SerialNumber"]);
+
+            foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Manufacturer, Product, Version from Win32_BaseBoard").Get())
+                _INFthisPC.Add((string)managementObj["Manufacturer"]+(string)managementObj["Product"]+", V"+ (string)managementObj["Version"]);
+
             foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name from Win32_Processor").Get())
                 _INFthisPC.Add((string)managementObj["Name"]);
-            foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name, AdapterRAM from Win32_VideoController").Get())
-            {
-                _INFthisPC.Add((string)managementObj["Name"]);
-                _INFthisPC.Add(Convert.ToString(((uint)managementObj["AdapterRAM"]/ 1048576)/1000));
+
+            foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select DeviceID from Win32_VideoController").Get())
+                _idVideoController.Add((string)managementObj["DeviceID"]);
+            for(int i=0; i<_idVideoController.Count; i++) { 
+                        foreach (var managementObj in new ManagementObjectSearcher("root\\cimv2", "select Name, AdapterRAM from Win32_VideoController where DeviceID='" + _idVideoController[i] + "'").Get())
+                        _test+=((string)managementObj["Name"] + ", " + Convert.ToString(((uint)managementObj["AdapterRAM"] / 1048576) / 1000) + " GB\n");
             }
+            _INFthisPC.Add(_test);
+
         }
 
         internal void SetInormationPC(SystemInfromation systemInfromation)
         {
-            systemInfromation.NameCPU.Text = _INFthisPC[0];
-            systemInfromation.NameGPU.Text = _INFthisPC[1] + ", " + _INFthisPC[2] + " GB";
+            systemInfromation.NameBIOS.Text = _INFthisPC[0];
+            systemInfromation.NameMotherBr.Text = _INFthisPC[1];
+            systemInfromation.NameCPU.Text = _INFthisPC[2];
+            systemInfromation.NameGPU.Text = _INFthisPC[3];
         }
     }
 }
