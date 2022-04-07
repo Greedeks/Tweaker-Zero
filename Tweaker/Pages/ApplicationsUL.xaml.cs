5ad2c7e29d0415734b896dfc8d457a5573cb5eb1
greedeks <greedeks@gmail.com>
2022-04-07 17:26:36 +0300

    discription

--- Tweaker/Pages/ApplicationsUL.xaml.cs
+++ Tweaker/Pages/ApplicationsUL.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tweaker.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для ApplicationsUL.xaml
-    /// </summary>
     public partial class ApplicationsUL : Page
     {
         public ApplicationsUL()
         {
             InitializeComponent();
         }
+
+        private void App_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Image _AppImage = (Image)sender;
+            Discription.Text = _AppImage.Name;
+        }
+
+        private void BingWeather_MouseLeave(object sender, MouseEventArgs e) => Discription.Text = "Наведите курсор на любое приложения, чтобы получить его название";
     }
 }
--- Tweaker/Properties/AssemblyInfo.cs
+++ Tweaker/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Windows;
 
 [assembly: AssemblyVersion("2.3")]
 [assembly: AssemblyInformationalVersion("2.3")]
-[assembly: AssemblyFileVersion("2.4.3.0")]
+[assembly: AssemblyFileVersion("2.4.5.0")]
 [assembly: NeutralResourcesLanguage("en")]
