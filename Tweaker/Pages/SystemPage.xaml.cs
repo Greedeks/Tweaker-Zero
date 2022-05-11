e26e3d3f471d95b9fb21d837b6da2b32fef12fdb
greedeks <greedeks@gmail.com>
2022-05-11 09:49:14 +0300

    Custom Slider

--- Tweaker/Pages/SystemPage.xaml.cs
+++ Tweaker/Pages/SystemPage.xaml.cs
@@ -12,17 +12,54 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Tweaker.Сlasses;
 
 namespace Tweaker.Pages
 {
-    /// <summary>
-    /// Логика взаимодействия для SystemPage.xaml
-    /// </summary>
     public partial class SystemPage : Page
     {
+        private readonly SettingsWindows _settingsWindows = new SettingsWindows();
+        private DispatcherTimer _timer = default;
+        private TimeSpan _time = TimeSpan.FromSeconds(0);
         public SystemPage()
         {
             InitializeComponent();
+
+            #region Update
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                if (_time.TotalSeconds % 60 == 0)
+                    //_settingsWindows.GetSettingServices(this);
+                _time = _time.Add(TimeSpan.FromSeconds(+1));
+            }, Application.Current.Dispatcher);
+            #endregion
         }
+
+        private void BtnOnOff_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Parallel.Invoke(() =>
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    Button btn = (Button)sender;
+                    if (btn.Name == "TweaksON")
+                    {
+                        for (byte _tweak = 1; _tweak <= 28; _tweak++)
+                            _settingsWindows.ChangeSettingServices(false, _tweak);
+                    }
+                    else
+                        for (byte _tweak = 1; _tweak <= 28; _tweak++)
+                            _settingsWindows.ChangeSettingServices(true, _tweak);
+
+                    //_settingsWindows.GetSettingServices(this);
+                    _timer.Start();
+                }
+            });
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e) => Parallel.Invoke(() => { /*/_settingsWindows.GetSettingServices(this);/*/ });
+
+        private void Page_Unloaded(object sender, RoutedEventArgs e) => _timer.Stop();
     }
 }
