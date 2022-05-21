714f1f2b9001ff72f7ed40f7790e5b10d2f3c856
greedeks <greedeks@gmail.com>
2022-05-21 11:33:43 +0300

    Hover Text - Tweaks Confidentiality Refactoring

--- Tweaker/Pages/ConfidentialityPage.xaml.cs
+++ Tweaker/Pages/ConfidentialityPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 using System.Windows.Threading;
 using Tweaker.Сlasses;
 
@@ -10,7 +11,8 @@ namespace Tweaker.Pages
 {
     public partial class ConfidentialityPage : Page
     {
-        private readonly SettingsWindows _settingsWindows = new SettingsWindows();
+        private readonly SettingsWindows settingsWindows = new SettingsWindows();
+        private readonly LanguageTranslate languageTranslate = new LanguageTranslate();
         private DispatcherTimer _timer = default;
         private TimeSpan _time = TimeSpan.FromSeconds(0);
 
@@ -22,20 +24,45 @@ namespace Tweaker.Pages
             _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
             {
                 if (_time.TotalSeconds % 60 == 0)
-                    _settingsWindows.GetSettingConfidentiality(this);
+                    settingsWindows.GetSettingConfidentiality(this);
                 _time = _time.Add(TimeSpan.FromSeconds(+1));
             }, Application.Current.Dispatcher);
             #endregion
+
+           DescriptionT.Text = languageTranslate.DescriptionTranslate();
+        }
+
+        private void DiscriptionAnim(string _text)
+        {
+            DescriptionT.Text = _text;
+
+            DoubleAnimationUsingKeyFrames doubleAnimation = new DoubleAnimationUsingKeyFrames();
+            EasingDoubleKeyFrame _fromFrame = new EasingDoubleKeyFrame(0)
+            {
+                KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0))
+            };
+
+            EasingDoubleKeyFrame _toFrame = new EasingDoubleKeyFrame(1)
+            {
+                KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(150))
+            };
+
+            doubleAnimation.KeyFrames.Add(_fromFrame);
+            doubleAnimation.KeyFrames.Add(_toFrame);
+            DescriptionT.BeginAnimation(ContextMenu.OpacityProperty, doubleAnimation);
         }
 
         private void Tweaks_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            Label label = (Label)sender;
+            DescriptionT.Style = (Style)Application.Current.Resources["DescriptionTweaks_View"];
+            DiscriptionAnim(languageTranslate.ConfidentialityTweaksHover(label.Name));
         }
 
         private void Tweaks_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            DescriptionT.Style = (Style)Application.Current.Resources["DescriptionTweaks"];
+            DiscriptionAnim(languageTranslate.DescriptionTranslate());
         }
 
         #region Tweaks
@@ -44,7 +71,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak1.Style = !TButton1.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton1.State, 1);
+                settingsWindows.ChangeSettingConfidentiality(TButton1.State, 1);
             }
         }
 
@@ -53,7 +80,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak2.Style = !TButton2.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton2.State, 2);
+                settingsWindows.ChangeSettingConfidentiality(TButton2.State, 2);
             }
         }
 
@@ -62,7 +89,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak3.Style = !TButton3.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton3.State, 3);
+                settingsWindows.ChangeSettingConfidentiality(TButton3.State, 3);
             }
         }
 
@@ -71,7 +98,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak4.Style = !TButton4.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton4.State, 4);
+                settingsWindows.ChangeSettingConfidentiality(TButton4.State, 4);
                 _timer.Start();
             }
         }
@@ -81,7 +108,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak5.Style = !TButton5.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton5.State, 5);
+                settingsWindows.ChangeSettingConfidentiality(TButton5.State, 5);
             }
         }
 
@@ -90,7 +117,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak6.Style = !TButton6.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton6.State, 6);
+                settingsWindows.ChangeSettingConfidentiality(TButton6.State, 6);
             }
         }
 
@@ -99,7 +126,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak7.Style = !TButton7.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton7.State, 7);
+                settingsWindows.ChangeSettingConfidentiality(TButton7.State, 7);
             }
         }
 
@@ -108,7 +135,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak8.Style = !TButton8.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton8.State, 8);
+                settingsWindows.ChangeSettingConfidentiality(TButton8.State, 8);
             }
         }
 
@@ -117,7 +144,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak9.Style = !TButton9.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton9.State, 9);
+                settingsWindows.ChangeSettingConfidentiality(TButton9.State, 9);
             }
         }
 
@@ -126,7 +153,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak10.Style = !TButton10.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton10.State, 10);
+                settingsWindows.ChangeSettingConfidentiality(TButton10.State, 10);
             }
         }
 
@@ -135,7 +162,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak11.Style = !TButton11.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton11.State, 11);
+                settingsWindows.ChangeSettingConfidentiality(TButton11.State, 11);
             }
         }
 
@@ -144,7 +171,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak12.Style = !TButton12.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton12.State, 12);
+                settingsWindows.ChangeSettingConfidentiality(TButton12.State, 12);
             }
         }
 
@@ -153,7 +180,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak13.Style = !TButton13.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton13.State, 13);
+                settingsWindows.ChangeSettingConfidentiality(TButton13.State, 13);
             }
         }
         private void TButton14_PreviewMouseDown(object sender, MouseButtonEventArgs e)
@@ -161,7 +188,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak14.Style = !TButton14.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton14.State, 14);
+                settingsWindows.ChangeSettingConfidentiality(TButton14.State, 14);
             }
         }
 
@@ -170,7 +197,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak15.Style = !TButton15.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton15.State, 15);
+                settingsWindows.ChangeSettingConfidentiality(TButton15.State, 15);
             }
         }
 
@@ -179,7 +206,7 @@ namespace Tweaker.Pages
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 Tweak16.Style = !TButton16.State ? (Style)Application.Current.Resources["Tweaks_ON"] : (Style)Application.Current.Resources["Tweaks_OFF"];
-                _settingsWindows.ChangeSettingConfidentiality(TButton16.State, 16);
+                settingsWindows.ChangeSettingConfidentiality(TButton16.State, 16);
             }
         }
         #endregion
@@ -194,19 +221,19 @@ namespace Tweaker.Pages
                     if (btn.Name == "TweaksON")
                     {
                         for (byte _tweak = 1; _tweak <= 16; _tweak++)
-                            _settingsWindows.ChangeSettingConfidentiality(false, _tweak);
+                            settingsWindows.ChangeSettingConfidentiality(false, _tweak);
                     }
                     else
                         for (byte _tweak = 1; _tweak <= 16; _tweak++)
-                            _settingsWindows.ChangeSettingConfidentiality(true, _tweak);
+                            settingsWindows.ChangeSettingConfidentiality(true, _tweak);
 
-                    _settingsWindows.GetSettingConfidentiality(this);
+                    settingsWindows.GetSettingConfidentiality(this);
                     _timer.Start();
                 }
             });
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e) => _settingsWindows.GetSettingConfidentiality(this);
+        private void Page_Loaded(object sender, RoutedEventArgs e) => settingsWindows.GetSettingConfidentiality(this);
 
         private void Page_Unloaded(object sender, RoutedEventArgs e) => _timer.Stop();
 
